622f45f1fb246484c8392aee280aee95febe2ad2
demura <kurdadzedemo@gmail.com>
2022-07-25 16:40:23 +0400

    postgre

--- mushakebi/mushakta_baza/admin.py
+++ mushakebi/mushakta_baza/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Departamenti,Tan_Mshromeli
+from .models import Departamenti, Mta_Dep,Tan_Mshromeli
 
 # Register your models here.
 admin.site.register(Departamenti)
 admin.site.register(Tan_Mshromeli)
+admin.site.register(Mta_Dep)
--- mushakebi/mushakta_baza/models.py
+++ mushakebi/mushakta_baza/models.py
@@ -2,23 +2,37 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+class Mta_Dep(models.Model):
+    Saxeli = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.Saxeli
+
 
 class Departamenti(models.Model):
     Saxeli=models.CharField(max_length=200)
+    
+    Mta_Dep=models.ForeignKey(Mta_Dep,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.Saxeli
+        return self.Mta_Dep.Saxeli + '/' +  self.Saxeli
 
 
 
 class Tan_Mshromeli(models.Model):
+    #პირადობაზე რაც წერია
     Saxeli = models.CharField(max_length=50)
     Gvari = models.CharField(max_length=50)
-    Emaili = models.EmailField()
+    Cit = models.CharField(max_length=50)
+    Sqesi = models.CharField(max_length=1)
     Piradi_Nom = models.CharField(max_length=11)
     Dab_Tar = models.DateField(default=datetime.now())
+    Vada = models.DateField(default=datetime.now())
+    Surati = models.ImageField()
+
+    Emaili = models.EmailField()
     Departamenti = models.ForeignKey(Departamenti,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.Saxeli
+        return self.Departamenti.Mta_Dep.Saxeli + '/' + self.Departamenti.Saxeli + '/' + self.Saxeli
 
